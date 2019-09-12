539a28e55c815910c5246c9cd244263f98fda101
Sandra Markerud <sandra.markerud@hornbach.com>
2019-09-12 13:12:39 +0200

    add dependencies for junit 5, h2 and jpa entity graph

--- src/test/java/com/markerud/recipes/RecipesApplicationTests.java
+++ src/test/java/com/markerud/recipes/RecipesApplicationTests.java
@@ -1,16 +1,16 @@
 package com.markerud.recipes;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
-public class RecipesApplicationTests {
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class RecipesApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
