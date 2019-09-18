d45deaa6587d19d50d2cf0fa3bf2af81d69ef1b8
Sandra Markerud <sandra.markerud@gmx.de>
2019-09-18 23:10:29 +0200

    add rudimentary header component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,19 @@
 import * as React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import {Route, Switch} from 'react-router-dom';
 import RecipeList from './components/recipeList/index';
+import Header from './components/shared/header/Header';
 
 const App: React.FC = () => {
-  return (
-      <React.Fragment>
-        <main>
-          <Switch>
-            <Route exact path="/" component={RecipeList} />
-          </Switch>
-        </main>
-      </React.Fragment>
-  );
+    return (
+        <React.Fragment>
+            <Header/>
+            <main>
+                <Switch>
+                    <Route exact path="/" component={RecipeList}/>
+                </Switch>
+            </main>
+        </React.Fragment>
+    );
 };
 
 export default App;
---
+++ frontend/src/components/shared/header/styles.ts
@@ -0,0 +1,12 @@
+import {colors} from "../../../config";
+
+export default {
+
+    headline: {
+        color: colors.standardColors.white,
+        backgroundColor: colors.standardColors.anthracite,
+        padding: '0.1em 1em',
+        margin: '0em',
+    },
+
+};
