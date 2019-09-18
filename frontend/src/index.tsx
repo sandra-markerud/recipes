cdf64c8bceabdd87bac32367c73cf114e25215ee
Sandra Markerud <sandra.markerud@gmx.de>
2019-09-18 22:49:35 +0200

    add normalize.css

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
 import App from './App';
+import 'normalize.css';
 import * as serviceWorker from './serviceWorker';
 
 const client = new ApolloClient({
