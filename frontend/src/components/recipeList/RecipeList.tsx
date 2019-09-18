cda773a01539923511b1c120b48191dcee49fb4c
Sandra Markerud <sandra.markerud@gmx.de>
2019-09-18 22:00:18 +0200

    correct import

--- frontend/src/components/recipeList/RecipeList.tsx
+++ frontend/src/components/recipeList/RecipeList.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import * as React from 'react';
 import { AllRecipesQuery } from '../../generated/graphql';
 import RecipeListEntry from './recipeListEntry/RecipeListEntry';
 import withStyles, { WithSheet } from 'react-jss';
