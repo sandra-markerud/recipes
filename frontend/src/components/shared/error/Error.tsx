ec890e894330b5649a527d87fdc859a1faca7267
Sandra Markerud <sandra.markerud@gmx.de>
2019-09-18 23:27:22 +0200

    add first styling for error component

--- frontend/src/components/shared/error/Error.tsx
+++ frontend/src/components/shared/error/Error.tsx
@@ -1,20 +1,20 @@
 import * as React from 'react';
-import { ApolloError } from 'apollo-boost';
-import withStyles, { WithSheet } from 'react-jss';
+import {ApolloError} from 'apollo-boost';
+import withStyles, {WithSheet} from 'react-jss';
 import styles from './styles';
 
 type ErrorProps = WithSheet<typeof styles, {}> & {
     error: ApolloError | undefined
 }
 
-const Error: React.FC<ErrorProps> = ( { error } ) => {
+const Error: React.FC<ErrorProps> = ({error, classes}) => {
     return (
-        <React.Fragment>
+        <div className={classes.errorDialog}>
             <h3>Oh nein, etwas ist schief gelaufen:</h3>
             {error && (
                 <p>{error.message}</p>
             )}
-        </React.Fragment>
+        </div>
     );
 };
 
--- frontend/src/components/shared/error/styles.ts
+++ frontend/src/components/shared/error/styles.ts
@@ -1,3 +1,16 @@
-export default  {
+import {colors} from "../../../config";
+
+export default {
+
+    errorDialog: {
+        border: {
+            color: colors.systemColors.error.dark,
+            style: 'solid',
+        },
+        backgroundColor: colors.systemColors.error.light,
+        color: colors.systemColors.error.dark,
+        padding: '0.5em 1em',
+        margin: '0.5em 1em',
+    },
 
 };
