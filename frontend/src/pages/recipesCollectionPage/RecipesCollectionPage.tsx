645eb8c50601bde16c4e2ade071fe5686d51d702
Sandra Markerud <sandra.markerud@gmx.de>
2019-09-23 13:28:35 +0200

    recipes are initially sorted by name

--- frontend/src/pages/recipesCollectionPage/RecipesCollectionPage.tsx
+++ frontend/src/pages/recipesCollectionPage/RecipesCollectionPage.tsx
@@ -10,7 +10,9 @@ type RecipesCollectionPageProps = WithSheet<typeof styles, {}> & {
 
 const RecipesCollectionPage: React.FC<RecipesCollectionPageProps> = ({data, classes}) => {
 
-    const listItems = data.allRecipes.map(currentRecipe => {
+    const listItems = data.allRecipes.sort((a, b) => {
+        return a.name === b.name ? 0 : a.name < b.name ? -1 : 1;
+    }).map(currentRecipe => {
         return (
             <RecipeTile key={currentRecipe.id} recipe={currentRecipe}/>
         );
