e01b6a07be8606bd08095aba7e79b34810c080b8
Sandra Markerud <sandra.markerud@gmx.de>
2019-09-24 21:15:20 +0200

    new recipe for sample data

--- src/main/resources/db/populate_db.sql
+++ src/main/resources/db/populate_db.sql
@@ -8,11 +8,22 @@ INSERT INTO UNITS (ID, CODE, NAME, VERSION, CREATED_ON, MODIFIED_ON) VALUES
 
 INSERT INTO FOODS (ID, NAME, VERSION, CREATED_ON, MODIFIED_ON) VALUES
 	(2000, 'Champignons', 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-	(2001, 'gekochter Schinken', 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+	(2001, 'Wacholderschinken', 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
 	(2002, 'Cherrytomaten', 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-	(2003, 'getrockneter Schnittlauch', 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+	(2003, 'Schnittlauch, getrocknet', 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
 	(2004, 'Zitronensaft', 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-	(2005, 'Nudeln', 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+	(2005, 'Nudeln', 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+	(2006, 'Oregano, getrocknet', 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+	(2007, 'Petersilie, getrocknet', 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+	(2008, 'Basilikum, getrocknet', 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+	(2009, 'Selleriesalz', 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+	(2010, 'Thymian, getrocknet', 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+	(2011, 'Bunter Pfeffer, gemahlen', 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+	(2012, 'Zwiebelpulver', 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+	(2013, 'Knoblauchpulver', 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+	(2014, 'Zucker', 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+	(2015, 'Salz', 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+	(2016, 'Paprikapulver, rosenscharf', 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
 
 INSERT INTO RECIPES (ID, NAME, INSTRUCTION, VERSION, CREATED_ON, MODIFIED_ON) VALUES
     (3000, 'Pilzpfanne in Zitronenrahm', 'Pilze schälen und in kleine Stücke schneiden. Gekochten Schinken in Würfel schneiden und zusammen mit den Pilzen anbraten...', 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
@@ -35,7 +46,16 @@ Does your lorem ipsum text long for something a little meatier? Give our generat
 	(3007, 'Flammkuchen Elsässer Art', 'Man nehme...', 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
 	(3008, 'Dosensuppe', 'Man nehme...', 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
 	(3009, 'Haferbrei à la Liv', 'Man nehme...', 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-	(3010, 'Müsli', 'Man nehme...', 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+	(3010, 'Fix ohne Fix: Italienische Salatkrönung nach Feinkostpunks Art', 'Alle Zutaten miteinander gut vermischen.
+
+In ein sauberes Glas geben - die Mischung hält sich vermutlich ewig, verliert aber sicher irgendwann an Geschmack. Ich würde versuchen, sie innerhalb eines Jahres aufzubrauchen,
+
+Für Salatdressing für 1 Person dosiert man so: 1 TL Mischung, 2 EL Balsamico, 1 EL Öl, 2 EL Wasser. Alles gut schütteln und am besten 5 Minuten stehen lassen - dann werden die getrockneten Kräuter etwas weicher, speziell beim Thymian finde ich das angenehm.  Das reicht für 1/2 Salatkopf.
+
+Optional: Wer mag gibt 1 TL Senf und 1 TL Honig in die angerührte Mischung hinzu. Das Dressing schmeckt aber auch ohne gut.
+
+Wichtig: vor dem Entnehmen der Mischung immer vorher im Glas gut durchrühren. Zucker und Salz neigen dazu, durch die Kräuter nach unten uz rieseln.', 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+
 
 INSERT INTO INGREDIENTS (ID, QUANTITY, UNIT_ID, FOOD_ID, RECIPE_ID, VERSION, CREATED_ON, MODIFIED_ON) VALUES
 	(4000, 600, 1000, 2000, 3000, 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
@@ -43,7 +63,20 @@ INSERT INTO INGREDIENTS (ID, QUANTITY, UNIT_ID, FOOD_ID, RECIPE_ID, VERSION, CRE
 	(4002, 12, 1001, 2002, 3000, 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
 	(4003, 3, 1002, 2003, 3000, 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
 	(4004, 1, 1005, 2004, 3000, 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+
 	(4005, 500, 1000, 2005, 3001, 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-	(4006, 200, 1000, 2001, 3001, 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
-	
+	(4006, 200, 1000, 2001, 3001, 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+
+	(4007, 3, 1002, 2006, 3010, 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+	(4008, 2, 1002, 2007, 3010, 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+	(4009, 1, 1002, 2008, 3010, 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+	(4010, 0.5, 1002, 2009, 3010, 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+	(4011, 0.5, 1002, 2010, 3010, 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+	(4012, 0.5, 1002, 2011, 3010, 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+	(4013, 2, 1002, 2012, 3010, 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+	(4014, 1, 1002, 2013, 3010, 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+	(4015, 2, 1002, 2014, 3010, 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+	(4016, 2, 1002, 2015, 3010, 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+	(4017, 0.5, 1002, 2016, 3010, 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+
 	
