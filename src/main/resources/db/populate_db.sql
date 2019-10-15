7635d77fbe38e7fc3c5574e6e8a8ef7819de1fce
Sandra Markerud <sandra.markerud@gmx.de>
2019-10-15 22:58:47 +0200

    improve test data

--- src/main/resources/db/populate_db.sql
+++ src/main/resources/db/populate_db.sql
@@ -4,7 +4,14 @@ INSERT INTO UNITS (ID, LONG_NAME, SHORT_NAME, VERSION, CREATED_ON, MODIFIED_ON) 
     (1002, 'Esslöffel', 'EL', 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
     (1003, 'Teelöffel', 'TL', 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
     (1004, 'Liter', 'l', 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-    (1005, 'Etwas', 'etwas', 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+    (1005, 'Kilogramm', 'kg', 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+    (1006, 'Becher', 'Becher', 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+    (1007, 'Würfel', 'Würfel', 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+    (1008, 'Etwas', 'etwas', 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+    (1009, 'Milliliter', 'ml', 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+    (1010, 'Päckchen', 'Päckchen', 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+    (1011, 'Bund', 'Bund', 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+    (1012, 'Glas', 'Glas', 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
 
 INSERT INTO FOODS (ID, NAME, VERSION, CREATED_ON, MODIFIED_ON) VALUES
 	(2000, 'Champignons', 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
@@ -23,30 +30,57 @@ INSERT INTO FOODS (ID, NAME, VERSION, CREATED_ON, MODIFIED_ON) VALUES
 	(2013, 'Knoblauchpulver', 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
 	(2014, 'Zucker', 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
 	(2015, 'Salz', 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-	(2016, 'Paprikapulver, rosenscharf', 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+	(2016, 'Paprikapulver, rosenscharf', 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+	(2017, 'Dinkelmehl', 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+	(2018, 'Haferflocken', 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+	(2019, 'Frischhefe', 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+	(2020, 'Kräutersalz', 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+	(2021, 'Basmatireis', 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+	(2022, 'Rinderbrühe', 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+	(2023, 'Rosmarin, getrocknet', 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+	(2024, 'Bunter Pfeffer, ganze Körner', 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+	(2025, 'Rinderhackfleisch', 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+	(2026, 'Zwiebel(n)', 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+	(2027, 'passierte Tomaten', 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+	(2028, 'geriebener Mozzarella', 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+	(2029, 'Paprikapulver, edelsüß', 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+	(2030, 'Cayennepfeffer', 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+	(2031, 'Meersalz', 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+	(2032, 'Nelken', 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+	(2033, 'Piment', 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+	(2034, 'Kreuzkümmel', 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+	(2035, 'Zimt', 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+	(2036, 'Kakaopulver', 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+	(2037, 'Wasser, lauwarm', 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+	(2038, 'Gemüsebrühe', 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+ 	(2039, 'Kochsahne', 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+ 	(2040, 'Schweineschnitzel', 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+ 	(2041, 'Frühlingszwiebeln', 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+ 	(2042, 'Gnocchi', 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+ 	(2043, 'Mehl', 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+ 	(2044, 'Bratengewürz', 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+ 	(2045, 'Wasser', 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+ 	(2046, 'Erbsen, tiefgekühlt', 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+ 	(2047, 'Pesto Verde', 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+ 	(2048, 'Château-Linsen', 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+ 	(2049, 'Kartoffeln', 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+ 	(2050, 'Karotten', 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+ 	(2051, 'Knollensellerie', 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+ 	(2052, 'Lauch', 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+ 	(2053, 'Balsamico Bianco', 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+ 	(2054, 'Petersilie', 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
 
 INSERT INTO RECIPES (ID, NAME, INSTRUCTION, VERSION, CREATED_ON, MODIFIED_ON) VALUES
-    (3000, 'Pilzpfanne in Zitronenrahm', 'Pilze schälen und in kleine Stücke schneiden. Gekochten Schinken in Würfel schneiden und zusammen mit den Pilzen anbraten...', 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-	(3001, 'Schinkennudeln', 'Nudeln bissfest kochen. Gekochter Schinken in Würfel schneiden und zusammen mit den Nudeln braten', 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-	(3002, 'Obstsalat', 'Alles kleinschnibbeln und mischen', 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-	(3003, 'Egg & Bacon', 'Bacon anbraten, warm stellen, danach Ei backen...', 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-	(3004, 'Bauerntopf', 'Man nehme...', 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-	(3005, 'Schokopudding', 'Man nehme...', 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-	(3006, 'Butterbrot', 'Spicy jalapeno bacon ipsum dolor amet turkey nisi filet mignon porchetta pork chop, pastrami magna. Corned beef in fugiat kevin nostrud meatball, frankfurter aliquip short ribs sirloin biltong alcatra. Corned beef in proident officia pastrami leberkas, commodo irure in in flank ham hock. Sirloin pork chop shank pastrami shoulder jerky burgdoggen hamburger porchetta ipsum. Filet mignon shankle venison occaecat ball tip cupidatat.
+    (3000, 'Reisauflauf (italienisch)', 'Etwas Öl in einer Pfanne erhitzen und den Reis langsam anbraten, bis er bräunlich ist.
 
-Officia veniam et in ut. Frankfurter doner capicola sausage, do corned beef excepteur voluptate tail hamburger andouille. Fatback ut minim, magna ex do occaecat boudin qui jowl hamburger eu prosciutto. Velit shank eiusmod, enim eu exercitation pancetta picanha leberkas t-bone laboris ut capicola. Laborum venison aute, velit picanha ham pork belly flank. Elit t-bone anim, ullamco pork belly pig ut qui. Venison aliqua shank sint tri-tip.
+Die Rinderbrühe, Oregano, Thymian, Rosmarin und Pfeffer dazugeben und ca. 10 Minuten bei offenem Deckel köcheln lassen, bis die Flüssigkeit verkocht ist.
 
-Pancetta ex boudin pork strip steak kielbasa tail velit dolore t-bone sint. Sausage brisket anim, adipisicing corned beef laborum ground round burgdoggen turkey kielbasa rump. Excepteur pig non, ullamco commodo t-bone in id exercitation venison qui sirloin. Quis adipisicing shoulder kielbasa, anim tail pancetta meatloaf irure. Velit cow spare ribs et ribeye officia buffalo jerky.
+Parallel hierzu in einer zweiten Pfanne etwas Öl erhitzen und Zwiebeln und Hackfleisch darin anbraten. Geschälte Tomaten dazugeben und verrühren.
 
-Irure elit sint beef ribs laborum sunt qui esse sausage proident quis buffalo. Et minim corned beef in. Veniam dolore dolor id pork loin, kielbasa ham hock salami eu occaecat lorem. Pork loin t-bone short ribs burgdoggen, enim sirloin pork chop officia ut ipsum ex. Porchetta enim excepteur, magna aliqua labore ut doner chicken reprehenderit laborum filet mignon commodo minim nulla. Consectetur pig minim pastrami pork chop ut. Minim reprehenderit ut pig consequat magna.
+Den Reis in einer Auflaufform verteilen. Das Hackfleisch darüber verteilen. Den gerieben Käse darüberbröseln.
 
-Eiusmod consequat beef ribs adipisicing. Cow ut qui, burgdoggen bresaola laborum dolore aliqua anim sunt deserunt tempor laboris reprehenderit. Lorem spare ribs magna, cupim rump chuck fatback swine quis filet mignon frankfurter beef ribs id do. T-bone ad tempor, proident shoulder labore excepteur magna.
-
-Does your lorem ipsum text long for something a little meatier? Give our generator a try… it’s tasty!', 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-	(3007, 'Flammkuchen Elsässer Art', 'Man nehme...', 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-	(3008, 'Dosensuppe', 'Man nehme...', 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-	(3009, 'Haferbrei à la Liv', 'Man nehme...', 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-	(3010, 'Fix ohne Fix: Italienische Salatkrönung nach Feinkostpunks Art', 'Alle Zutaten miteinander gut vermischen.
+Im Herd bei 200°C ca. 1/2 Stunde backen.', 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+    (3001, 'Würzmischung: Italienische Salatkrönung', 'Alle Zutaten miteinander gut vermischen.
 
 In ein sauberes Glas geben - die Mischung hält sich vermutlich ewig, verliert aber sicher irgendwann an Geschmack. Ich würde versuchen, sie innerhalb eines Jahres aufzubrauchen,
 
@@ -54,29 +88,121 @@ Für Salatdressing für 1 Person dosiert man so: 1 TL Mischung, 2 EL Balsamico, 
 
 Optional: Wer mag gibt 1 TL Senf und 1 TL Honig in die angerührte Mischung hinzu. Das Dressing schmeckt aber auch ohne gut.
 
-Wichtig: vor dem Entnehmen der Mischung immer vorher im Glas gut durchrühren. Zucker und Salz neigen dazu, durch die Kräuter nach unten zu rieseln.', 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+Wichtig: vor dem Entnehmen der Mischung immer vorher im Glas gut durchrühren. Zucker und Salz neigen dazu, durch die Kräuter nach unten zu rieseln.', 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+    (3002, 'Würzmischung: Chili con Carne', '' ||
+     'Beide Sorten Pfeffer, Nelken und Piment zusammen in einer Gewürzmühle oder mit dem Mörser fein zerkleinern.
 
+Kakaopulver und Zimtpulver miteinander verrühren
+
+Und jetzt alles hübsch in ein Glas stapeln:
+ - die Hälfte Paprika
+ - die Hälfte vom Pfeffer-Mix
+ - den Kakao-Mix
+ - Kreuzkümmel
+ - Cayennepfeffer
+ - Oregano
+ - Salz
+ - die zweite Hälfte vom Pfeffer-Mix
+ - die zweite Hälfte Paprika
+
+Optisch so schön gestapelt eignet sich die Würzmischung gut zum Verschenken. Vor dem ersten Grbrauch muss die Würzmischung jedoch gut durschgemischt werden.', 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+    (3003, 'Dinkelbrötchen Waldorfkindergarten', 'Das Mehl mit den Haferflocken, dem lauwarmen Wasser, der Frischhefe un ddem Kräutersalz vermischen und den Teig gut durchkneten.
+
+Den Teig an einem warmen Ort ca. 30-45 Minuten gehen lassen.
+
+Aus dem Teig kleine Brötchen formen.
+
+Ein Glas Wasser mit in den Backofen stellen und die Brötchen bei 200°C Ober- und Unterhitze ca. 20 Minuten backen.', 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+    (3004, 'Pilzpfanne in Zitronenrahm', 'Pilze putzen und je nach Größe vierteln oder halbieren.
+
+Öl in einer großen Pfanne erhitzen und die Pilze darin goldbraun braten. Bei Bedarf die Pilze in mehreren Portionen anbraten, damit sie nicht in ihrem eigenen Wasser kochen.
+
+Kirschtomaten zugeben.
+
+Mit Brühe und Kochsahne ablöschen, mit Pfeffer würzen und ein paar Minuten dicklich einkochen.
+
+Schnittlauch unter die Pilze rühren.
+
+Mit Zitronensaft abschmecken.', 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+    (3005, 'Gnocchipfanne mit Schnitzelstreifen', 'Schnitzel trocken tupfen und in dünne Streifen schneiden. Mit etwas Öl und dem Bratengewürz marinieren. Frühlingszwiebeln putzen, waschen und in feine Ringe schneiden.
+
+Öl in einer großen Pfanne erhitzen. Gnocchi darin rundherum goldbraun anbraten.
+
+In einer zweiten Pfanne Öl erhitzen. Fleisch bei starker Hitze darin rundherum braun anbraten. Mehl darüberstäuben und kurz anschwitzen. Mit Wasser und Kochsahne ablöschen und unter Rühren aufkochen.
+
+Gefrorene Erbsen und die Frühlingszwiebeln einrühren, wieder aufkochen und ca. 3 Minuten köcheln lassen. Das Pesto unterrühren.', 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+    (3006, 'Linsensuppe', 'Möhre, Sellerie und Lauch in klitzekleine Würfel schneiden. Die Kartoffeln in Würfel mit einer Kantenlänge nicht über 5 mm schneiden.
+
+Die Linsen waschen, abtropfen lassen und mit der Gemüsebrühe 30 Minuten köcheln lassen.
+
+In einem zweiten Topf das Öl erhitzen und das Gemüse mit den Kartoffeln kurz anschwitzen. Mit der Rinderbrühe ablöschen und köcheln lassen. Die Linsen dazugeben und alles gut mischen. Mit Essig abschmecken.
+
+Petersilie fein hacken und die gefüllten Teller damit garnieren.', 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
 
 INSERT INTO INGREDIENTS (ID, QUANTITY, UNIT_ID, FOOD_ID, RECIPE_ID, VERSION, CREATED_ON, MODIFIED_ON) VALUES
-	(4000, 600, 1000, 2000, 3000, 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-	(4001, 100, 1000, 2001, 3000, 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-	(4002, 12, 1001, 2002, 3000, 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-	(4003, 3, 1002, 2003, 3000, 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-	(4004, 1, 1005, 2004, 3000, 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+	(4000, 150, 1000, 2021, 3000, 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+	(4001, 0.5, 1004, 2022, 3000, 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+	(4002, 1, 1003, 2006, 3000, 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+	(4003, 1, 1003, 2010, 3000, 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+	(4004, 1, 1003, 2023, 3000, 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+	(4005, 0.5, 1003, 2011, 3000, 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+	(4006, 500, 1000, 2025, 3000, 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+	(4007, 1, 1001, 2026, 3000, 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+	(4008, 400, 1009, 2027, 3000, 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+	(4009, 1, 1010, 2028, 3000, 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
 
-	(4005, 500, 1000, 2005, 3001, 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-	(4006, 200, 1000, 2001, 3001, 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+	(4010, 3, 1002, 2006, 3001, 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+	(4011, 2, 1002, 2007, 3001, 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+	(4012, 1, 1002, 2008, 3001, 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+	(4013, 0.5, 1002, 2009, 3001, 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+	(4014, 0.5, 1002, 2010, 3001, 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+	(4015, 0.5, 1002, 2011, 3001, 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+	(4016, 2, 1002, 2012, 3001, 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+	(4017, 1, 1002, 2013, 3001, 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+	(4018, 2, 1002, 2014, 3001, 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+	(4019, 2, 1002, 2015, 3001, 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+	(4020, 0.5, 1002, 2016, 3001, 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
 
-	(4007, 3, 1002, 2006, 3010, 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-	(4008, 2, 1002, 2007, 3010, 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-	(4009, 1, 1002, 2008, 3010, 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-	(4010, 0.5, 1002, 2009, 3010, 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-	(4011, 0.5, 1002, 2010, 3010, 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-	(4012, 0.5, 1002, 2011, 3010, 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-	(4013, 2, 1002, 2012, 3010, 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-	(4014, 1, 1002, 2013, 3010, 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-	(4015, 2, 1002, 2014, 3010, 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-	(4016, 2, 1002, 2015, 3010, 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-	(4017, 0.5, 1002, 2016, 3010, 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+	(4021, 8, 1002, 2029, 3002, 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+	(4022, 2, 1002, 2030, 3002, 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+	(4023, 4, 1002, 2006, 3002, 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+	(4024, 4, 1002, 2031, 3002, 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+	(4025, 4, 1002, 2024, 3002, 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+	(4026, 1, 1003, 2032, 3002, 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+	(4027, 2, 1002, 2033, 3002, 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+	(4028, 3, 1002, 2034, 3002, 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+	(4029, 0.5, 1003, 2035, 3002, 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+	(4030, 2, 1002, 2036, 3002, 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
 
-	
+	(4031, 1, 1005, 2017, 3003, 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+	(4032, 2, 1006, 2018, 3003, 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+	(4033, 600, 1009, 2037, 3003, 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+	(4034, 1, 1007, 2019, 3003, 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+	(4035, 2, 1003, 2020, 3003, 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+
+	(4036, 600, 1000, 2000, 3004, 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+	(4037, 12, 1001, 2002, 3004, 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+	(4038, 100, 1009, 2038, 3004, 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+	(4039, 200, 1009, 2039, 3004, 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+	(4040, 2, 1002, 2008, 3004, 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+	(4041, 1, 1008, 2004, 3004, 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+
+	(4042, 500, 1000, 2040, 3005, 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+	(4043, 1, 1011, 2041, 3005, 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+	(4044, 500, 1000, 2042, 3005, 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+	(4045, 1, 1002, 2043, 3005, 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+	(4046, 1, 1002, 2044, 3005, 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+	(4047, 200, 1009, 2045, 3005, 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+	(4048, 200, 1009, 2039, 3005, 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+	(4049, 150, 1000, 2046, 3005, 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+	(4050, 0.5, 1012, 2047, 3005, 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+
+	(4051, 200, 1000, 2048, 3006, 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+	(4052, 300, 1000, 2049, 3006, 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+	(4053, 100, 1000, 2050, 3006, 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+	(4054, 100, 1000, 2051, 3006, 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+	(4055, 100, 1000, 2052, 3006, 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+	(4056, 500, 1009, 2038, 3006, 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+	(4057, 500, 1009, 2022, 3006, 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+	(4058, 1, 1002, 2053, 3006, 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+	(4059, 1, 1011, 2054, 3006, 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
