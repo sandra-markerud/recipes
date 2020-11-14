d3662d2e4e8c7fd590be51114d732b6eb70825a3
Sandra Markerud <sandra.markerud@gmx.de>
2020-11-14 22:20:02 +0100

    use ResponseStatusException and remove not needed controller advice

--- src/main/java/com/markerud/recipes/ui/RecipesController.java
+++ src/main/java/com/markerud/recipes/ui/RecipesController.java
@@ -1,45 +1,47 @@
 package com.markerud.recipes.ui;
 
-import static com.markerud.recipes.ViewConstants.RECIPES_DETAIL_PAGE;
-import static com.markerud.recipes.ViewConstants.RECIPES_LIST_PAGE;
-import static com.markerud.recipes.persistence.RecipeRepository.recipeDetailEntityGraph;
-
-import java.util.Optional;
-
-import javax.persistence.EntityNotFoundException;
-
-import org.springframework.beans.factory.annotation.Autowired;
+import com.markerud.recipes.model.Recipe;
+import com.markerud.recipes.persistence.RecipeRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.markerud.recipes.model.Recipe;
-import com.markerud.recipes.persistence.RecipeRepository;
+import java.util.Optional;
 
-import lombok.RequiredArgsConstructor;
+import static com.markerud.recipes.ViewConstants.RECIPES_DETAIL_PAGE;
+import static com.markerud.recipes.ViewConstants.RECIPES_LIST_PAGE;
+import static com.markerud.recipes.persistence.RecipeRepository.recipeDetailEntityGraph;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 @Controller
+@RequiredArgsConstructor
 @RequestMapping("/recipes")
-@RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class RecipesController {
 
-	private final RecipeRepository recipeRepo;
+    private final RecipeRepository recipeRepo;
 
-	@GetMapping
-	public ModelAndView showRecipesListPage() {
-		ModelAndView mav = new ModelAndView(RECIPES_LIST_PAGE);
-		mav.addObject("recipes", recipeRepo.findAllProjectedBy());
-		return mav;
-	}
+    @GetMapping
+    public ModelAndView showRecipesListPage() {
+        ModelAndView mav = new ModelAndView(RECIPES_LIST_PAGE);
+        mav.addObject("recipes", recipeRepo.findAllProjectedBy());
+        return mav;
+    }
 
-	@GetMapping("/detail/{id}")
-	public ModelAndView showRecipesDetailPage(@PathVariable("id") Long id) {
-		ModelAndView mav = new ModelAndView(RECIPES_DETAIL_PAGE);
-		Optional<Recipe> potentialRecipe = recipeRepo.findById(id, recipeDetailEntityGraph);
-		mav.addObject("recipe", potentialRecipe.orElseThrow(EntityNotFoundException::new));
-		return mav;
-	}
+    @GetMapping("/detail/{id}")
+    public ModelAndView showRecipesDetailPage(@PathVariable("id") Long id) {
+        ModelAndView mav = new ModelAndView(RECIPES_DETAIL_PAGE);
+        Optional<Recipe> potentialRecipe = recipeRepo.findById(id, recipeDetailEntityGraph);
+        mav.addObject("recipe", potentialRecipe.orElseThrow(() -> new ResponseStatusException(NOT_FOUND)));
+        return mav;
+    }
 
 }
+
+
+
+
+
