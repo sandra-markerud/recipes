60c2ee1e5980102ae467aa58b6e12131c54b2de1
Sandra Markerud <sandra.markerud@gmx.de>
2020-11-15 22:59:40 +0100

    redesign layout

--- src/main/java/com/markerud/recipes/config/BeanConfig.java
+++ src/main/java/com/markerud/recipes/config/BeanConfig.java
@@ -2,7 +2,9 @@ package com.markerud.recipes.config;
 
 import nz.net.ultraq.thymeleaf.LayoutDialect;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class BeanConfig {
 
     @Bean
